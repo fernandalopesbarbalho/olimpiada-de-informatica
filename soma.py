f9f9269f86b260b16ff94848880d27618261c375
fernandalopesbarbalho <137642560+fernandalopesbarbalho@users.noreply.github.com>
2023-06-25 13:15:43 -0300

    Add files via upload

---
+++ soma.py
@@ -0,0 +1,6 @@
+#Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. Imprima X conforme exemplo apresentado abaixo.
+
+num1 = int(input())
+num2 = int(input())
+
+print("X =", num1 + num2)
