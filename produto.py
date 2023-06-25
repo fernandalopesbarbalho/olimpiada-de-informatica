f81e727a078efd7a70252ac7c901d5b2870b6728
fernandalopesbarbalho <137642560+fernandalopesbarbalho@users.noreply.github.com>
2023-06-25 13:22:47 -0300

    Add files via upload

---
+++ produto.py
@@ -0,0 +1,6 @@
+#Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. A seguir mostre a variável PROD com mensagem correspondente.   
+
+num1 = int(input())
+num2 = int(input())
+
+print("PROD =", num1 * num2)
